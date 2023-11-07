c6d654eafed3106d81fb8a89eea32dda838667b9
Atik <atik_mahbub_m-tsapp1ma2223@cev.com>
2023-11-07 22:07:28 +0100

    Commit inicial

---
+++ ver_datos.py
@@ -0,0 +1,5 @@
+from supabase import create_client, Client
+import json
+
+url = "https://secxumjiywgzbfdwafsc.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlY3h1bWppeXdnemJmZHdhZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MDc4MjgsImV4cCI6MjAxMjA4MzgyOH0.wJUWAsPsIVZoba2OSRAPXUJmexJRjz0t7dSyyxPPDmw"
