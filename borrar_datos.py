d0cb7aeaa282eaf1532aa9d5db8c936d873fa238
Atik <atik_mahbub_m-tsapp1ma2223@cev.com>
2023-11-07 23:04:07 +0100

    Le pregunta al usuario que fila de dato quiere borrar y para la confirmacion tiene que poner el ID de la fila de datos y lo ejecuta

--- borrar_datos.py
+++ borrar_datos.py
@@ -34,3 +34,31 @@ for x in range(len(campos)):
 
 print('Esta es la lista de los campos.')
 print(campoIndividual)
+
+nombre_campo = input('Escribe el nombre de un campo del que quieres borrar la fila entera: ')
+valorCampo = input('Cual es el valor del campo?')
+
+response = supabase.table(tabla).select("*").eq(nombre_campo, valorCampo).execute()
+datos_filtrados = list(json.loads(response.model_dump_json())["data"])
+print(datos_filtrados)
+
+borrar_id = input('Selecciona el id que quieres borrar: ')
+
+if tabla == "users":
+    response = supabase.from_('events').update({'user_id': None}).match({'user_id': borrar_id}).execute()
+    response = supabase.from_('comments').update({'user_id': None}).match({'user_id': borrar_id}).execute()
+    time.sleep(1.5)
+elif tabla == "tag":
+    response = supabase.from_('events').update({'tag_id': None}).match({'tag_id': borrar_id}).execute()
+    response = supabase.from_('calendars').update({'tag_id': None}).match({'tag_id': borrar_id}).execute()
+    time.sleep(1.5)
+elif tabla == "events":
+    response = supabase.from_('comments').update({'event_id': None}).match({'event_id': borrar_id}).execute()
+    response = supabase.from_('calendars').update({'event_id': None}).match({'event_id': borrar_id}).execute()
+    time.sleep(1.5)
+elif tabla == "calendars":
+    response = supabase.from_('users').update({'calendar_id': None}).match({'calendar_id': borrar_id}).execute()
+    time.sleep(1.5)
+
+    
+response = supabase.table(tabla).delete().eq(campos[0], borrar_id).execute()
