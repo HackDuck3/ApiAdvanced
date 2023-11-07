46614671aa0ca15abc558f9bd4da75d137710376
Atik <atik_mahbub_m-tsapp1ma2223@cev.com>
2023-11-07 22:35:41 +0100

    Creamos lista de tablas y preguntamos en que tabla quiere insertar los datos

--- insertar_datos.py
+++ insertar_datos.py
@@ -8,3 +8,17 @@ url = "https://secxumjiywgzbfdwafsc.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlY3h1bWppeXdnemJmZHdhZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MDc4MjgsImV4cCI6MjAxMjA4MzgyOH0.wJUWAsPsIVZoba2OSRAPXUJmexJRjz0t7dSyyxPPDmw"
 
 supabase = create_client(url ,key)
+
+listaTablas = ["tag", "events", "calendars", "comments",  "users"]
+
+while buscando:
+    tabla = input('En que tabla quieres introducir los datos? \n - users \n - events \n - calendars \n - comments \n - tag \n')
+    for campoTabla in listaTablas:
+        
+        if tabla.lower() == campoTabla:
+            buscando = False
+
+    if buscando != False:
+        print("No se encontro la tabla, corrige si esta mal escrito")
+
+response = supabase.table(tabla).select("*").execute()
