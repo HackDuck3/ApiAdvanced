dcdc1d2d998d0f94b925b907cc857df9d5efb3a2
Atik <atik_mahbub_m-tsapp1ma2223@cev.com>
2023-11-07 23:08:43 +0100

    Filtra para saber en que tabla y que campo quiere modificar el usuario

--- modificar_datos.py
+++ modificar_datos.py
@@ -5,3 +5,16 @@ url = "https://secxumjiywgzbfdwafsc.supabase.co/"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlY3h1bWppeXdnemJmZHdhZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MDc4MjgsImV4cCI6MjAxMjA4MzgyOH0.wJUWAsPsIVZoba2OSRAPXUJmexJRjz0t7dSyyxPPDmw"
 
 supabase = create_client(url, key)
+
+#Por ejemplo, para listar los datos de una tabla:
+table_name = input("Introduce el nombre de la tabla a modificar: \n -tag \n -calendars \n -comments \n -events \n -users \n ")
+
+response = supabase.table(table_name).select("*").execute()
+
+campos = list(json.loads(response.model_dump_json())["data"][0].keys())
+campoIndividual = ""
+for x in range(len(campos)):
+    campoIndividual += "-"+campos[x]+"\n"
+
+print('Esta es la lista de los campos en: '+table_name)
+print(campoIndividual)
