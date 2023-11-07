77494f94f21b7a66159d1d4270d601beaae2c4b1
Atik <atik_mahbub_m-tsapp1ma2223@cev.com>
2023-11-07 22:34:16 +0100

    Commit inicial del segundo ejercicio conectando con supabase (insertar_datos)

---
+++ insertar_datos.py
@@ -0,0 +1,10 @@
+from supabase import create_client, Client
+import json
+
+buscando = True
+patata = True
+
+url = "https://secxumjiywgzbfdwafsc.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlY3h1bWppeXdnemJmZHdhZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MDc4MjgsImV4cCI6MjAxMjA4MzgyOH0.wJUWAsPsIVZoba2OSRAPXUJmexJRjz0t7dSyyxPPDmw"
+
+supabase = create_client(url ,key)
