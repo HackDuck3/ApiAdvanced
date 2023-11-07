fbc85017694e22fc4087e9d290a9363746bc84eb
Atik <atik_mahbub_m-tsapp1ma2223@cev.com>
2023-11-07 22:16:44 +0100

    Preguntamos de que tabla quiere ver los datos y mostramos

--- ver_datos.py
+++ ver_datos.py
@@ -5,3 +5,9 @@ url = "https://secxumjiywgzbfdwafsc.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlY3h1bWppeXdnemJmZHdhZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1MDc4MjgsImV4cCI6MjAxMjA4MzgyOH0.wJUWAsPsIVZoba2OSRAPXUJmexJRjz0t7dSyyxPPDmw"
 
 supabase = create_client(url ,key)
+
+tabla = input('Que tabla quieres ver? ')
+
+response = supabase.table(tabla).select('*').execute()
+
+print(response)
