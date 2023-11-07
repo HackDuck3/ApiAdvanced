4d72b5453b64b5fe8a71b503d98a06de4099d618
Atik <atik_mahbub_m-tsapp1ma2223@cev.com>
2023-11-07 23:01:14 +0100

    Busca la tabla y le muestra la lista de los campos

--- borrar_datos.py
+++ borrar_datos.py
@@ -12,3 +12,25 @@ supabase = create_client(url ,key)
 
 listaTablas = ["tag", "events", "calendars", "comments",  "users"]
 listaForanea = ["user_id", "tag_id", "calendar_id", "event_id", "comment_id"]
+
+#Esta preguntando al usuario y buscando el nombre de la tabla
+while buscando:
+    tabla = input('En que tabla quieres introducir los datos? \n - users \n - events \n - calendars \n - comments \n - tag \n')
+    for campoTabla in listaTablas:
+        #Filtra el nombre que ha escrito el usuario
+        if tabla.lower() == campoTabla:
+            buscando = False
+
+    if buscando != False:
+        print("No se encontro la tabla, corrige si esta mal escrito")
+
+response = supabase.table(tabla).select("*").execute()
+
+
+campos = list(json.loads(response.model_dump_json())["data"][0].keys())
+campoIndividual = ""
+for x in range(len(campos)):
+    campoIndividual += "-"+campos[x]+"\n"
+
+print('Esta es la lista de los campos.')
+print(campoIndividual)
